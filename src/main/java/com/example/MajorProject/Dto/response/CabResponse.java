de6d6142de70ea1517dedb76e1ba5d964eeee345
Shashank pant <sumit0000pant@gmail.com>
2026-05-28 16:18:43 +0530

    Completed Cab feature

--- src/main/java/com/example/MajorProject/Dto/response/CabResponse.java
+++ src/main/java/com/example/MajorProject/Dto/response/CabResponse.java
@@ -1,14 +1,12 @@
 package com.example.MajorProject.Dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class CabResponse {
     private String cabNumber;
 
