b1d03b6c1dfda8fdc177067a269c1734fefdfd4c
Shashank pant <sumit0000pant@gmail.com>
2026-05-28 18:33:02 +0530

    Added exception in bookCab API

--- src/main/java/com/example/MajorProject/controller/BookingController.java
+++ src/main/java/com/example/MajorProject/controller/BookingController.java
@@ -2,6 +2,7 @@ package com.example.MajorProject.controller;
 
 import com.example.MajorProject.Dto.request.BookingRequest;
 import com.example.MajorProject.Dto.response.BookingResponse;
+import com.example.MajorProject.exception.CustomerNotFound;
 import com.example.MajorProject.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ public class BookingController {
 
     @PostMapping("/book/customer/{id}")
         public BookingResponse bookCab(@RequestBody BookingRequest bookingRequest,
-                                       @PathVariable("id") int customerId){
+                                       @PathVariable("id") int customerId) throws CustomerNotFound {
                 return bookingService.bookCab(bookingRequest, customerId);
 
         }
